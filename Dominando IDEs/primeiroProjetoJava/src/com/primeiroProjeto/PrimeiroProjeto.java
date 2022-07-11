e06805637c15bf52bc5dd078275020effa32bd58
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-07-11 19:28:12 -0300

    Primeiro commit

---
+++ Dominando IDEs/primeiroProjetoJava/src/com/primeiroProjeto/PrimeiroProjeto.java
@@ -0,0 +1,16 @@
+package com.primeiroProjeto;
+
+import com.primeiroProjeto.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		/*int a = 5;
+		int b = 10;
+		System.out.println("Hello World! " + (a+b));*/
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+	}
+
+}
