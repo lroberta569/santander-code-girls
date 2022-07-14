4223b3e78d5f4d272c7050275aa8e3a9e8f2183e
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-07-14 13:47:08 -0300

    Criação e Sobrecargas - Exercicios

---
+++ Métodos Java/MetodosJava/src/br/com/exercicios/Main.java
@@ -0,0 +1,34 @@
+package br.com.exercicios;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Calculadora
+		System.out.println("Exercicio Calculadora");
+		Calculadora.soma(3, 6);
+		Calculadora.subtracao(9, 1.8);
+		Calculadora.multiplicacao(7, 8);
+		Calculadora.divisao(5, 2.5);
+		
+		//Mensagem
+		System.out.println("Exercicio Mensagem");
+		Mensagem.obterMensagem(9);
+		Mensagem.obterMensagem(14);
+		Mensagem.obterMensagem(1);
+		
+		//Empréstimo
+		System.out.println("Exercicio Empréstimo");
+		Emprestimo.Calcular(1000, Emprestimo.getDuasParcelas());
+		Emprestimo.Calcular(1000, Emprestimo.getTresParcelas());
+		Emprestimo.Calcular(1000, 5);
+		
+		//Quadrilatero
+		System.out.println("Exercicio Quadrilátero");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5d,5d);
+		Quadrilatero.area(7, 8, 9);
+		Quadrilatero.area(5f,5f);
+	}
+
+}
