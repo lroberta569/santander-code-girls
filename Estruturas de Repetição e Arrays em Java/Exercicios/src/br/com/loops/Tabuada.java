d414c3d9584a9afc1da2ce517aae70cec3e88534
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-07-23 00:46:45 -0300

    Exercicios - Estruturas de repetição e Arrays

---
+++ Estruturas de Repetição e Arrays em Java/Exercicios/src/br/com/loops/Tabuada.java
@@ -0,0 +1,36 @@
+package br.com.loops;
+
+import java.util.Scanner;
+
+/*
+ * Desenvolva um gerador de tabuada,
+ * capaz de gerar a tabuada  de qualquer numero inteiro entre 1 e 10.
+ * O usuário deve informar de qual numero ele deseja ver a tabuada.
+ * A saída deve ser conforme exemplo abaixo:
+ * 
+ * Tabuada do 5:
+ * 5 x 1 = 5
+ * 5 x 2 = 10
+ * ...
+ * 5 x 10 = 50;
+ */
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Tabuada: ");
+		int tabuada  = leia.nextInt();
+		
+		System.out.println("Tabuada do " + tabuada);
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(tabuada + " X " + i + " = " + tabuada * i);
+			
+		}
+	
+
+	}
+
+}
