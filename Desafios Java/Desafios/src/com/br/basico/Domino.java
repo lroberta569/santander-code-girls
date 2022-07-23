4f989a0c108b89b35aeb587718a5410f4086c903
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-07-23 14:04:32 -0300

    Desafios

---
+++ Desafios Java/Desafios/src/com/br/basico/Domino.java
@@ -0,0 +1,18 @@
+package com.br.basico;
+
+import java.util.Scanner;
+
+public class Domino {
+	public static void main(String[] args) {
+		
+	
+	Scanner leitor = new Scanner(System.in);
+	
+	int n = leitor.nextInt();
+	
+    System.out.println((n+1) * (n+2) / 2);
+    	
+	}
+    
+
+}
